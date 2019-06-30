319c614359b685de1d5b535872d8581b8d3b2e5c
r888800009 <r888800009@gmail.com>
2019-06-30 19:38:14 +0800

    Roughly completed farming navigate ai

--- farm-tower/drone.lua
+++ farm-tower/drone.lua
@@ -10,8 +10,17 @@ local entrance_len_y = 0
 -- entrance y axis value
 -- note the y is world y not waypoint y
 local farm_entrance_data = {
-  117, 111, 107, 101, 97, 91, 87, 81
+  { y = 117, down = 6, downOffset = -1 },
+  { y = 111, down = 6, downOffset = 1 },
+  { y = 107, down = 6, downOffset = -1 },
+  { y = 101, down = 6, downOffset = 1 },
+  { y = 97, down = 6, downOffset = -1 },
+  { y = 91, down = 6, downOffset = 1 },
+  { y = 87, down = 6, downOffset = -1 },
+  { y = 81, down = 6, downOffset = 1 }
 }
+
+local farm_w, farm_h = 16, 11
 
 -- offset is farmland x: 0 z: 0 relative entrance
 local farm_offset_x = 0
@@ -26,39 +35,56 @@ function correctionXYZ()
   nav_x, nav_y, nav_z = nav.getPosition()
 end
 
-function move(x, y, z, absolute)
+function move(x, y, z, absolute, error_limit)
   local absolute = absolute or false
-
+  local error_limit = error_limit or 0.1
   if absolute then
     correctionXYZ()
     x, y, z = x - nav_x, y - nav_y, z - nav_z
   end
 
   drone.move(x, y, z)
-  while drone.getOffset() > 0.1 or drone.getVelocity() > 0.01 do
+  while drone.getOffset() > error_limit or drone.getVelocity() > 0.01 do
     drone.setStatusText(tostring(drone.getOffset()))
     computer.pullSignal(0.2)
   end
 end
 
-function farming()
-  local dx, dy, dz = 1, 0, 0
+function reverse(xyz)
+  return { x = -xyz.x, y = -xyz.y, z = -xyz.z}
+end
+
+-- axis1 always at same level, length is farm_w
+-- axis2 like a stairs, length is farm_h
+-- increase is a vector
+-- ex: increase_axis1 = { x = 1, y = 0, z = 0 }
+-- inc1: increase axis1
+-- inc2: increase axis2
+
+function farming(inc1, inc2, stair_down_axis2, stair_offset)
+  drone.move(farm_offset_x, 0 , farm_offset_z)
   local delay = 0.2
   drone.select()
-  for x = 0, 5, 1
+  for axis2 = 1, farm_h, 1
   do
     drone.place(0)
-    for y = 0, 5, 1
+    for axis1 = 1, farm_w - 1, 1
     do
-      drone.move(dx, dy, dz)
-     computer.pullSignal(delay)
+      drone.move(inc1.x, inc1.y, inc1.z)
+      computer.pullSignal(delay)
       drone.place(0)
-
     end
-    dx, dy, dz = -dx, -dy, -dz
+    inc1 = reverse(inc1)
 
-    drone.move(0, 0, 1)
-    computer.pullSignal(delay)
+    if axis2 ~= farm_h then
+      drone.move(inc2.x, inc2.y, inc2.z)
+      computer.pullSignal(delay)
+    end
+
+    if axis2 == stair_down_axis2 then
+      drone.move(0, stair_offset, 0)
+      computer.pullSignal(delay)
+    end
   end
 end
 
@@ -105,6 +131,11 @@ function traversingFram()
     -- into farm
     drone.setLightColor(0x00ff00)
     move(entrance_len_x, entrance_len_y, entrance_len_z)
+
+    -- farm
+    savePos()
+    farming({ x = -1, y = 0, z = 0 }, { x = 0, y = 0, z = -1 }, value.down, value.downOffset)
+    restorePos()
 
     -- out farm
     drone.setLightColor(0x0000ff)
