e1c97cbd796bf8fec43c7d6990f0b880e8ae7241
r888800009 <r888800009@gmail.com>
2019-06-30 16:17:15 +0800

    Add move function

--- farm-tower/drone.lua
+++ farm-tower/drone.lua
@@ -1,5 +1,27 @@
 
 local drone = component.proxy(component.list("drone")())
+local nav = component.proxy(component.list("navigation")())
+
+local nav_x, nav_y, nav_z
+
+function correctionXYZ()
+  nav_x, nav_y, nav_z = nav.getPosition()
+end
+
+function move(x, y, z, absolute)
+  local absolute = absolute or false
+
+  if absolute then
+    correctionXYZ()
+    x, y, z = x - nav_x, y - nav_y, z - nav_z
+  end
+
+  drone.move(x, y, z)
+  while drone.getOffset() > 0.1 or drone.getVelocity() > 0.01 do
+    drone.setStatusText(tostring(drone.getOffset()))
+    computer.pullSignal(0.2)
+  end
+end
 
 function farming()
   local dx, dy, dz = 1, 0, 0
