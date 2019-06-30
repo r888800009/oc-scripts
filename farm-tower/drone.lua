b011d2fa982d98a36ce1378983645dbd67b4302f
r888800009 <r888800009@gmail.com>
2019-06-30 16:31:39 +0800

    Add traversingFram() function

--- farm-tower/drone.lua
+++ farm-tower/drone.lua
@@ -2,7 +2,24 @@
 local drone = component.proxy(component.list("drone")())
 local nav = component.proxy(component.list("navigation")())
 
+-- conifg
+local entrance_len_x = -3
+local entrance_len_z = 0
+local entrance_len_y = 0
+
+-- entrance y axis value
+-- note the y is world y not waypoint y
+local fram_entrance_data = {
+  117, 111, 107, 101, 97, 91, 87, 81
+}
+
+-- offset is farmland x: 0 z: 0 relative entrance
+local fram_offset_x = 0
+local fram_offset_z = 1
+
 local nav_x, nav_y, nav_z
+local wp_range = 256
+local wp_name = "Robot Pipe"
 
 function correctionXYZ()
   nav_x, nav_y, nav_z = nav.getPosition()
@@ -43,3 +60,33 @@ function farming()
     computer.pullSignal(delay)
   end
 end
+
+function initWaypoint()
+  local wps = nav.findWaypoints(wp_range)
+  for i = 1, wps.n do
+    if wps[i].label == wp_name then
+      move(wps[i].position[1], wps[i].position[2], wps[i].position[3])
+      correctionXYZ()
+    end
+  end
+end
+
+function init()
+  initWaypoint()
+end
+
+function traversingFram()
+  for key, value in pairs(fram_entrance_data) do
+    drone.setLightColor(0xff0000)
+    move(nav_x, value + 0.5, nav_z, true)
+
+    drone.setLightColor(0x00ff00)
+    move(entrance_len_x, entrance_len_y, entrance_len_z)
+
+    drone.setLightColor(0x0000ff)
+    move(-entrance_len_x, -entrance_len_y, -entrance_len_z)
+  end
+end
+
+init()
+traversingFram()
